1d357b8834186e6125166e0a3e366a097f5dd59b
Issadeenbaseem <issadeenbaseem95@gmail.com>
2024-07-18 23:20:38 +0400

    Routes and Pages

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -1,8 +1,23 @@
+
 import React from 'react'
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import Profile from './pages/Profile'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import About from './pages/About'
+import Home from './pages/Home'
 
 const App = () => {
   return (
-    <div className='text-red-800'>App</div>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home/>}  />
+      <Route path='profile' element={<Profile/>} />
+      <Route path='sign-in' element={<SignIn/>} />
+      <Route path='sign-up' element={<SignUp/>} />
+      <Route path='about' element={<About/>} />
+    </Routes>
+    </BrowserRouter>
   )
 }
 
