ae1897a4cede4a85472320258943f581d06eab14
Issadeenbaseem <issadeenbaseem95@gmail.com>
2024-07-21 23:02:36 +0400

    Mongo DB connection setup

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,7 +1,16 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
 
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_KEY).then(() =>{
+    console.log("Mogo_DB connection Successful")
+}).catch((error)=>{
+        console.log(error)
+})
 const app = express();
 
 app.listen(4000,() =>{
-    console.log("Server running Port 4000 !")
+    console.log("Server running Port 4000")
 })
